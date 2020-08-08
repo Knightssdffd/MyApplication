7d5a4c4758a450b5c4e569860af7d1a5afaa363b
幻忧 <837639041@qq.com>
2020-08-08 11:04:05 +0800

    text1

--- app/src/main/java/com/aksy/myapplication/MainActivity.java
+++ app/src/main/java/com/aksy/myapplication/MainActivity.java
@@ -2,21 +2,11 @@ package com.aksy.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.LinearInterpolator;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         relativeLayout.setOnTouchListener(new OnDoubleClickListener(new OnDoubleClickListener.MyClickCallBack() {
             @Override
             public void oneClick() {
+                Toast.makeText(MainActivity.this, "aaaaaaaaaaaa", Toast.LENGTH_SHORT).show();
             }
 
             @Override
